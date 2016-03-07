ef6a409e84371f503dd89f180ab9d4a7c4fc9331
vladvaldtitov <uplight.ca@gmail.com>
2016-03-06 21:03:05 -0500

    First

--- rem/statistics.php
+++ rem/statistics.php
@@ -1,7 +1,9 @@
 <?php
 $out = new StdClass();
-$csv = 'http://107.170.97.252/IS&S/OakvilleDashboard/js/ajax/Oakville_public/HelpDeskScreen.csv';
-$screen = file_get_contents($csv);
-$out->screen =  str_getcsv($screen);
-echo json_encode($out);
+$db= new PDO('sqlite:../data/status2.db');
+
+$sql = 'select * from status order by id desc limit 100';
+$res = $db->query($sql);
+if($res) $res = $res->fetchAll(PDO::FETCH_OBJ);
+echo json_encode($res);
 ?>
