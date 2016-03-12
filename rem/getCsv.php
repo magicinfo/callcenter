eafbb36a8585908f78c2b18f1a55fe777356a250
vladvaldtitov <uplight.ca@gmail.com>
2016-03-12 15:49:33 -0500

    remove files

--- rem/getCsv.php
+++
@@ -1,9 +0,0 @@
-<?php
-$url ='http://107.170.97.252/IS&S/OakvilleDashboard/js/ajax/Oakville_public/HelpDeskScreen.csv';
-
-$csv = file_get_contents($url);
-echo $csv;
-//$ar = str_getcsv($csv);
-//header('Content-type: application/json');
-
-//echo json_encode($ar);
